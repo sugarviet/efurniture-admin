12c8c82a67d66c830143e696ec38c84f1389b231
Viet Dang <vietdanghoang1705@gmail.com>
2023-12-12 17:12:03 +0700

    add search bar

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -2,7 +2,7 @@
 const Navbar = () => {
 
   return (
-    <nav id="navbar" className="flex justify-between primary_background w-full h-full">
+    <nav id="navbar" className="flex justify-between primary_background w-full h-full text-white">
       <h1>Navbar</h1>
     </nav>
   );
--- src/pages/Products/index.jsx
+++ src/pages/Products/index.jsx
@@ -1,40 +1,90 @@
-import React from 'react';
-import { Card, List } from 'antd';
-const { Meta } = Card;
+import {
+  EditOutlined,
+  EllipsisOutlined,
+  SettingOutlined,
+} from "@ant-design/icons";
+import { Avatar, Card, List, Input } from "antd";
+import { useState } from "react";
 
+const { Meta } = Card;
+const { Search } = Input;
+
+const productList = [
+  {
+    id: 1,
+    name: "Haha",
+    price: "$19.99",
+    manufacturer: "Manufacturer 1",
+    image: "https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png",
+  },
+  {
+    id: 2,
+    name: "Product 2",
+    price: "$29.99",
+    manufacturer: "Manufacturer 2",
+    image: "https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png",
+  },
+  {
+    id: 3,
+    name: "Product 2",
+    price: "$29.99",
+    manufacturer: "Manufacturer 2",
+    image: "https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png",
+  },
+  {
+    id: 4,
+    name: "Product 2",
+    price: "$29.99",
+    manufacturer: "Manufacturer 2",
+    image: "https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png",
+  },
+];
 const Products = () => {
-  const productList = [
-    {
-      id: 1,
-      name: 'Product 1',
-      price: '$19.99',
-      manufacturer: 'Manufacturer 1',
-      image: 'https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png',
-    },
-    {
-      id: 2,
-      name: 'Product 2',
-      price: '$29.99',
-      manufacturer: 'Manufacturer 2',
-      image: 'https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png',
-    },
-  ];
+  const [filteredProducts, setFilteredProducts] = useState(productList);
+
+  const handleSearch = (e) => {
+    const filtered = productList.filter((product) =>
+      product.name.toLowerCase().includes(e.target.value.toLowerCase())
+    );
+    setFilteredProducts(filtered);
+  };
 
   return (
     <div>
+      <Search
+        placeholder="Search products"
+        onChange={handleSearch}
+        style={{ marginBottom: 16 }}
+      />
       <List
-        grid={{ gutter: 16, column: 3 }}
-        dataSource={productList}
+        grid={{ gutter: 16, column: 4 }}
+        dataSource={filteredProducts}
         renderItem={(item) => (
           <List.Item>
             <Card
               hoverable
               style={{
-                width: 240,
+                width: 300,
               }}
-              cover={<img alt={item.name} src={item.image} />}
+              cover={
+                <img
+                  alt="example"
+                  src="https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png"
+                />
+              }
+              actions={[
+                <SettingOutlined key="setting" />,
+                <EditOutlined key="edit" />,
+                <EllipsisOutlined key="ellipsis" />,
+              ]}
             >
-              <Meta title={item.name} description={`Price: ${item.price} | Manufacturer: ${item.manufacturer}`} />
+              <Meta
+                avatar={
+                  <Avatar src="https://xsgames.co/randomusers/avatar.php?g=pixel" />
+                }
+                title="Card title"
+                description="This is the description"
+              />
             </Card>
           </List.Item>
         )}
