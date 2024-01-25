a3e1e610af800043c31ccc974a6c00440d3e8330
nobi-onway <115331625+nobita-onway@users.noreply.github.com>
2024-01-25 18:38:48 +0700

    create: ecommerce status component

---
+++ src/constants/icons/index.js
@@ -0,0 +1,9 @@
+import onProcessing from '../../images/on_processing.png';
+import outOfStock from '../../images/out_of_stock.png';
+import onHold from '../../images/on_hold.png';
+
+export const ICONS = {
+    onProcessing: onProcessing,
+    outOfStock: outOfStock,
+    onHold: onHold,
+}
