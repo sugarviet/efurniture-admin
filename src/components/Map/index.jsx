76e35976200e356ebd193f7a7b3dc9139bbbfbad
nobi-onway <115331625+nobita-onway@users.noreply.github.com>
2024-01-30 16:04:34 +0700

    create: most purchased map location

---
+++ src/components/Map/index.jsx
@@ -0,0 +1,46 @@
+import Map, { Marker } from "react-map-gl";
+import { COORDINATES } from "../../constants/enums";
+import PropTypes from "prop-types";
+
+function MapBox({ locations }) {
+  return (
+    <Map
+      mapboxAccessToken="pk.eyJ1Ijoibm9iaXRhODkiLCJhIjoiY2xyajRxMGVnMDVuajJrcW41aGFtYzh5YSJ9.1A258o2oKsYxbYY8Qfx2yQ"
+      initialViewState={{
+        longitude: COORDINATES.get("TP HCM").longitude,
+        latitude: COORDINATES.get("TP HCM").latitude,
+        zoom: 10,
+      }}
+      mapStyle="mapbox://styles/nobita89/clrn549cu004j01o3h8f38nmr"
+      style={{ width: "100%", height: "100%" }}
+    >
+      {locations.map((location) => {
+        const { district, volume } = location;
+        const { latitude, longitude } = COORDINATES.get(district);
+        return (
+          <Marker key={district} latitude={latitude} longitude={longitude}>
+            <section className="flex flex-col items-center">
+              <div className="relative w-fit flex items-center justify-center aspect-square p-2 rounded-full bg-black">
+                <span className="font-semibold text-center text-white">
+                  {volume}
+                </span>
+                <div className="rounded-full absolute -right-2 -left-2 -top-2 -bottom-2 bg-black opacity-20 -z-10"></div>
+              </div>
+              <span className="font-bold">{district}</span>
+            </section>
+          </Marker>
+        );
+      })}
+    </Map>
+  );
+}
+
+export default MapBox;
+
+MapBox.propTypes = {
+  locations: PropTypes.arrayOf(PropTypes.object),
+};
+
+MapBox.defaultProps = {
+  locations: [],
+};
