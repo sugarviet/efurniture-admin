0758d99c6a7bb3ca7c424dca1597d465169cfc04
Viet Dang <vietdanghoang1705@gmail.com>
2024-03-12 20:58:39 +0700

    create flashsale

--- src/pages/CategoryDetail/index.jsx
+++ src/pages/CategoryDetail/index.jsx
@@ -1,22 +1,5 @@
 import { Divider } from "antd";
-import HorizontalList from "@components/HorizontalList";
-import ProductCard from "@components/ProductCard";
 import PageTitle from "@components/PageTitle";
-
-const data = [
-  {
-    title: "Title 1",
-  },
-  {
-    title: "Title 2",
-  },
-  {
-    title: "Title 3",
-  },
-  {
-    title: "Title 4",
-  },
-];
 
 const CategoryDetail = () => {
   return (
@@ -25,7 +8,7 @@ const CategoryDetail = () => {
 
       <Divider />
       <h2 className="text-xl">List products</h2>
-      <HorizontalList cols={4} data={data} dataItem={ProductCard} />
+      
     </div>
   );
 };
